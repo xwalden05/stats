60df4ff53f9b8164bd69d13a51a193ea863758ae
Xavier Walden <walde154@umn.edu>
2020-10-15 23:12:59 -0500

    added unit tests for median function

--- test_descr_stats.py
+++ test_descr_stats.py
@@ -1,5 +1,6 @@
 import unittest
 from descr_stats import mean_stat
+from descr_stats import median_stat
 
 class TestMeanStat(unittest.TestCase):
     ### Checks median_stat function.
@@ -11,6 +12,21 @@ class TestMeanStat(unittest.TestCase):
 
     def test_mean3(self):
         self.assertEqual(mean_stat([-1,4,.5,2.5]),1.5)
+
+class TestMedianStat(unittest.TestCase):
+    ### Checks median_stat function.
+    def test_median1(self):
+        self.assertEqual(median_stat([5,3,17]),5)
+
+    def test_median2(self):
+        self.assertEqual(median_stat([-7]),-7)
+
+    def test_median3(self):
+        self.assertEqual(median_stat([2, 1, 11, 8, 5, 2]),3.5)
+
+    def test_median4(self):
+        self.assertEqual(median_stat([-1, 4, .5, 2.5]),1.5)
+
 
 if __name__ == '__main__':
     unittest.main()
